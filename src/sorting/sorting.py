0e6486570d65f9e06d91f187a0a36d6469ad6b95
rowaishanna <rowaishanna@gmail.com>
2020-10-06 23:03:36 -0400

    sorting complete

--- src/sorting/sorting.py
+++ src/sorting/sorting.py
@@ -1,18 +1,44 @@
-# TO-DO: complete the helper function below to merge 2 sorted arrays
+# TO-DO: complete the helper function below to merge 2 SORTED arrays
 def merge(arrA, arrB):
-    elements = len(arrA) + len(arrB)
-    merged_arr = [0] * elements
+    c = [] # final output/merged array
+    ai, bi = 0, 0 # track the index of a and b
+    # as long as both ai and bi are less than 
+    # the length of either of the arrays
+    while ai < len(arrA) and bi < len(arrB):
+        # compare the two values at each index
+        # in each array
+        if arrA[ai] < arrB[bi]:
+            # if the value at ai is less than
+            # the value at bi, append the value at 
+            # ai to c and add one to the index
+            c.append(arrA[ai])
+            ai += 1
+        else:
+            c.append(arrB[bi])
+            bi += 1
+    # when we reach the end of one of the arrays
+    # we extend the output array with the remaining 
+    # values from the other array
+    if ai == len(arrA):
+        c.extend(arrB[bi:])
+    else:
+        c.extend(arrA[ai:])
 
-    # Your code here
-
-
-    return merged_arr
+    return c
 
 # TO-DO: implement the Merge Sort function below recursively
 def merge_sort(arr):
-    # Your code here
-
-
+    # merge sort continually splits lists down
+    # to one element
+    
+    if len(arr) > 1:
+        # split the list in half and call merge sort recursively 
+        left = merge_sort(arr[:len(arr)//2])
+        right = merge_sort(arr[len(arr)//2:])
+        # once we have arrays of length 1
+        # merge the now sorted sublists
+        arr = merge(left, right)
+     
     return arr
 
 # STRETCH: implement the recursive logic for merge sort in a way that doesn't 
@@ -20,9 +46,8 @@ def merge_sort(arr):
 # In other words, your implementation should not allocate any additional lists 
 # or data structures; it can only re-use the memory it was given as input
 def merge_in_place(arr, start, mid, end):
-    # Your code here
+    pass
 
 
 def merge_sort_in_place(arr, l, r):
-    # Your code here
-
+    pass
